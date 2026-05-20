263400fa03ee380823b6c0fcc10f0cd2cbf8fcf7
jenniferfelixmanicure-creator <jenniferfelixmanicure@gmail.com>
2026-05-20 15:05:01 -0300

    feat: schema index - exporta novas tabelas

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -1,6 +1,10 @@
 export * from "./users";
-export * from "./categories";
-export * from "./rides";
-export * from "./ratings";
-export * from "./wallet";
-export * from "./messages";
+  export * from "./categories";
+  export * from "./rides";
+  export * from "./ratings";
+  export * from "./wallet";
+  export * from "./messages";
+  export * from "./sos_events";
+  export * from "./saved_addresses";
+  export * from "./subscription_payments";
+  
